a9bb0b323d194115fa8001ec46c97dcc59b4003f
Tran Dinh Vu <trandinhvu1805@gmail.com>
2024-10-24 11:02:46 +0700

    edit post revision interface

--- lib/src/interface/post_revision.dart
+++ lib/src/interface/post_revision.dart
@@ -1,36 +1,30 @@
 import '../../wordpress_client.dart';
 
-/// Represents the interface for interacting with WordPress posts.
+/// Represents the interface for interacting with WordPress posts revisions.
 ///
-/// This class provides methods for creating, retrieving, updating, deleting,
+/// This class provides methods for creating, retrieving, deleting,
 /// and listing posts in a WordPress site.
 ///
 /// Example usage:
 ///
 /// ```dart
 /// final wordpress = WordpressClient(baseUrl: 'https://your-wordpress-site.com');
-/// final postsInterface = wordpress.posts;
+/// final postRevisionInterface = wordpress.postRevision;
 ///
-/// // Create a new post
-/// final newPost = await postsInterface.create(CreatePostRequest(
-///   title: 'My New Post',
-///   content: 'This is the content of my new post.',
+/// // Create a new post revisions.
+/// final newPostRevision = await postRevisionInterface.create(CreatePostRequest(
+///   title: 'My New Post revisions.',
+///   content: 'This is the content of my new post revisions.',
 ///   status: 'publish',
 /// ));
 ///
-/// // Retrieve a post
+/// // Retrieve a post revisions.
 /// final post = await postsInterface.retrieve(RetrievePostRequest(id: 123));
 ///
-/// // Update a post
-/// final updatedPost = await postsInterface.update(UpdatePostRequest(
-///   id: 123,
-///   title: 'Updated Post Title',
-/// ));
-///
-/// // Delete a post
+/// // Delete a post revisions.
 /// await postsInterface.delete(DeletePostRequest(id: 123));
 ///
-/// // List posts
+/// // List post revisions.
 /// final posts = await postsInterface.list(ListPostRequest(
 ///   perPage: 10,
 ///   page: 1,
@@ -41,5 +35,4 @@ final class PostRevisionInterface extends IRequestInterface
         CreateOperation<PostRevision, CreatePostRevisionRequest>,
         DeleteOperation<DeletePostRevisionRequest>,
         RetrieveOperation<PostRevision, RetrievePostRevisionRequest>,
-        UpdateOperation<PostRevision, UpdatePostRequest>,
         ListOperation<PostRevision, ListPostRequest> {}
